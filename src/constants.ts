57e25874ee6dc7ba4ea1085bc27281812396858d
duskdusts <czy@live.hk>
2025-04-20 23:32:21 +0800

    666

--- src/constants.ts
+++ src/constants.ts
@@ -11,7 +11,7 @@ import { SITE } from "@/config";
 export const SOCIALS = [
   {
     name: "Github",
-    href: "https://github.com/duskdust",
+    href: "https://github.com/duskdusts",
     linkTitle: ` ${SITE.title} on Github`,
     icon: IconGitHub,
   },
