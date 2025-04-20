49de4cb45927f3ba1e0820af389526f6bd9311c3
duskdusts <czy@live.hk>
2025-04-20 23:29:12 +0800

    666

--- src/constants.ts
+++ src/constants.ts
@@ -11,25 +11,13 @@ import { SITE } from "@/config";
 export const SOCIALS = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/duskdust",
     linkTitle: ` ${SITE.title} on Github`,
     icon: IconGitHub,
   },
   {
-    name: "X",
-    href: "https://x.com/username",
-    linkTitle: `${SITE.title} on X`,
-    icon: IconBrandX,
-  },
-  {
-    name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
-    linkTitle: `${SITE.title} on LinkedIn`,
-    icon: IconLinkedin,
-  },
-  {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:czy@live.hk",
     linkTitle: `Send an email to ${SITE.title}`,
     icon: IconMail,
   },
